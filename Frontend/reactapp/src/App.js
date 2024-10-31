7e761940ce874c12c3416ce0bc745cea77548137
benmalekaya <142318086+ayabenmalek@users.noreply.github.com>
2024-10-31 19:25:36 +0100

    updated

---
+++ Frontend/reactapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <div className="text-center bg-red-500 text-white p-4">
+      <h1 className="text-3xl font-bold">Bienvenue sur mon application React avec Tailwind CSS !</h1>
+    </div>
+    </div>
+  );
+}
+
+export default App;
