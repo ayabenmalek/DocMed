03ad030b86321ddd40a601085541d8895cdc7d84
manelderrouaz <derrouazmanel@gmail.com>
2024-12-14 22:15:49 +0100

    update

--- Backend/docmed/docmedapp/views.py
+++ Backend/docmed/docmedapp/views.py
@@ -13,12 +13,14 @@ class SearchEcritApiView(APIView):
     def get(self, request): 
         ecrit_type = request.query_params.get('type')  # Get the type (article, cours, thèse)
 
+
         if ecrit_type not in ['article', 'cours', 'these']:
             return Response({"error": "Invalid type. Valid options are 'article', 'cours', or 'thèse'."},
                             status=status.HTTP_400_BAD_REQUEST)
 
         # Filter by type and search query (if provided)
         results = Ecrit.objects.filter(type=ecrit_type)
+
 
         serializer = EcritSerializer(results, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
