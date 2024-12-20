e6bfc41993a40ea8cecaad382ee56b6f4e4b9435
manelderrouaz <derrouazmanel@gmail.com>
2024-12-20 19:13:34 +0100

    fetching comments feature

--- Backend/docmed/docmedapp/urls.py
+++ Backend/docmed/docmedapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SearchEcritApiView
+from .views import SearchEcritApiView,GetComments
 
 urlpatterns = [
     path('search-ecrit/', SearchEcritApiView.as_view(), name='search-ecrit'),
+    path('getComments/',GetComments.as_view(), name='getComments')
 ]
 
 
--- Backend/docmed/docmedapp/views.py
+++ Backend/docmed/docmedapp/views.py
@@ -5,8 +5,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Ecrit
-from .serializers import EcritSerializer
+from .models import Ecrit,Comment
+from .serializers import EcritSerializer,CommentSerializer
 
 
 class SearchEcritApiView(APIView): 
@@ -15,11 +15,11 @@ class SearchEcritApiView(APIView):
         search_query = request.query_params.get('q')
 
         if ecrit_type not in ['article', 'cours', 'these']:
-            return Response({"error": "Invalid type. Valid options are 'article', 'cours', or 'thèse'."},
+            return Response({"error": "Invalid type. Valid options are 'article', 'cours', or 'these'."},
                             status=status.HTTP_400_BAD_REQUEST)
         
         results = Ecrit.objects.filter(type=ecrit_type)
-        
+
         if search_query:
             results = results.filter(titre__icontains=search_query)
 
@@ -27,4 +27,10 @@ class SearchEcritApiView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
             
 
+
+class GetComments(APIView):
+    def get(self, request, *args, **kwargs):
+        results = Comment.objects.all()
+        dataSerializers = CommentSerializer(results,many=True)
+        return Response({'status':'success','data':dataSerializers.data },status=200)
 
