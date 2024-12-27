5a285f135f2ebb1ccda14a9d1790af90067732f2
Mey1921 <meyayachikhi@gmail.com>
2024-12-27 14:50:23 -0800

    added to views

--- Backend/docmed/docmedapp/views.py
+++ Backend/docmed/docmedapp/views.py
@@ -22,3 +22,88 @@ class SearchEcritApiView(APIView):
 
         serializer = EcritSerializer(results, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+#Mey's
+
+class EcritApiView(APIView):
+    def get(self, request, ecrit_id):
+        try:
+            if not ecrit_id:
+                raise AuthenticationFailed('Ecrit ID not provided')
+            ecrit = Ecrit.objects.get(ecrit_id=ecrit_id)
+            serializer = EcritSerializer(ecrit)
+            return Response([serializer.data], status=status.HTTP_200_OK)
+        except Ecrit.DoesNotExist:
+            return Response({'error': 'Ecrit not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+
+
+class EcritsEcrivainApiView(APIView):
+    def get(self, request, ecrivain_id):
+        try:
+            if not ecrivain_id:
+                raise AuthenticationFailed('Ecrivain ID not provided')
+            ecrivain = Ecrivain.objects.get(ecrivain_id=ecrivain_id)
+
+            ecrits = Ecrit.objects.filter(ecrivain=ecrivain).values('titre', 'type')
+
+            return Response(list(ecrits), status=status.HTTP_200_OK)
+        except Ecrivain.DoesNotExist:
+            return Response({"error": "Ecrivain not found."}, status=status.HTTP_404_NOT_FOUND)
+
+
+
+class InfoEcrivainApiView(APIView):
+    def get(self, request, ecrivain_id):
+        try:
+            if not ecrivain_id:
+                raise AuthenticationFailed('Ecrivain ID not provided')
+            ecrivain = Ecrivain.objects.get(ecrivain_id=ecrivain_id)
+            serializer = EcrivainSerializer(ecrivain)
+            return Response([serializer.data], status=status.HTTP_200_OK)
+        except Ecrivain.DoesNotExist:
+            return Response({'error': 'Ecrivain not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+class AddEcritApiView(APIView):
+    def post(self, request):
+        serializer = EcritSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Added successfully'}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AddEcrivainApiView(APIView):
+    def post(self, request):
+        serializer = EcrivainSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Added successfully'}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteEcritApiView(APIView):
+    def delete(self, request, ecrit_id):
+        try:
+            ecrit = Ecrit.objects.get(ecrit_id=ecrit_id)
+            ecrit.delete()
+            return Response({'message': 'Deleted successfully'}, status=status.HTTP_200_OK)
+        except Ecrit.DoesNotExist:
+            return Response({'error': 'Ecrit not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class EditEcritApiView(APIView):
+    def put(self, request, ecrit_id):
+        try:
+            ecrit = Ecrit.objects.get(ecrit_id=ecrit_id)
+            serializer = EcritSerializer(ecrit, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'message': 'Updated successfully'}, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Ecrit.DoesNotExist:
+            return Response({'error': 'Ecrit not found.'}, status=status.HTTP_404_NOT_FOUND)
+
